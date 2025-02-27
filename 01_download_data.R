8af0c8067665fb17952150190d4ffc6a3b7abdf0
Abby Hardin-Kohli <a.hardin-kohli@utah.edu>
2025-02-27 15:27:51 -0700

    implemented weather data downloading
    1. Moved the code for visualizing the target data that Madeline wrote to the .rmd file
    
    2. Added code to download historical weather data and find daily averages
    
    3. Did the same for weather forecast data

--- 01_download_data.R
+++ 01_download_data.R
@@ -1,9 +1,50 @@
 library(tidyverse)
+
+our_site <- "BARC"
+
+# Download aquatics targets data
 url <- "https://sdsc.osn.xsede.org/bio230014-bucket01/challenges/targets/project_id=neon4cast/duration=P1D/aquatics-targets.csv.gz"
 aquatics_targets <- read_csv(url, show_col_types = FALSE) #download data from NEON for all aquatic (?seems to be missing a couple?) sites
-aquatics_targets |>     #graph the two sites through time
-  filter(site_id == 'BLWA') |>  #pull data from BLWA site
-  ggplot(aes(x = datetime, y = observation)) +
-  geom_point(size = .5) +
-  facet_grid(variable~site_id, scales = "free_y") +
-  theme_bw()
+
+# Pull just the data from study site
+aquatics_targets <- aquatics_targets |> 
+                      filter(site_id == our_site)
+
+
+
+
+
+# Define which weather variables we are interested in
+weather_variables <- c("surface_downwelling_longwave_flux_in_air",
+                      "surface_downwelling_shortwave_flux_in_air",
+                      "precipitation_flux")
+
+# Download historical weather data
+historical_weather <- neon4cast::noaa_stage3()
+historical_weather <- historical_weather |> 
+                        filter(site_id == our_site,
+                                      variable %in% weather_variables) |>
+                        collect()
+# Aggregate to daily mean
+historical_weather <- historical_weather |>
+                        mutate(datetime = as.Date(datetime)) |>
+                          group_by(datetime, site_id, variable) |> 
+                          summarise(daily_mean = mean(prediction)) |>
+                          ungroup()
+
+
+                          
+# Download forecasted weather data
+yest_date <- Sys.Date() - lubridate::days(1) # Need to use yesterdays forecast (today's not available yet)
+weather_forecast <- neon4cast::noaa_stage2(start_date = yest_date)
+weather_forecast <- weather_forecast |> 
+                      filter(datetime >= Sys.Date(),
+                             site_id == our_site,
+                             variable %in% weather_variables) |>
+                      collect()
+# Aggregate to daily mean
+weather_forecast <- weather_forecast |>
+                      mutate(datetime = as.Date(datetime)) |>
+                      group_by(datetime, site_id, variable) |> 
+                      summarise(daily_mean = mean(prediction)) |>
+                      ungroup()
